2c0cab3aef731ed68bfb209d344f78bd79f1b689
nakulpandey <nakul.pandey@gmail.com>
2017-08-03 13:51:44 +0530

    session-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderNavigationBarComponent} from "./components/header-navigation-bar/header-navigation-bar.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavigationBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/header-navigation-bar/header-navigation-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-navigation-bar',
+  templateUrl: './header-navigation-bar.component.html',
+  styleUrls: ['./header-navigation-bar.component.css']
+})
+export class HeaderNavigationBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
