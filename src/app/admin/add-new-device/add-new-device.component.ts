34d7c13c88fb7c0d6bde78eab91cb1bbe2f59ca8
nakulpandey <nakul.pandey@gmail.com>
2017-08-24 15:11:23 +0530

    WIP

---
+++ src/app/admin/add-new-device/add-new-device.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-device',
+  templateUrl: './add-new-device.component.html',
+  styleUrls: ['./add-new-device.component.css']
+})
+export class AddNewDeviceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
